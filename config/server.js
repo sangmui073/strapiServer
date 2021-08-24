5b4456231c65ee270911afb979c9f8938f8d539b
TM-Phuc <sangmui073@gmail.com>
2021-08-24 12:54:10 +0700

    commic

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '15e235855ae15c8d251f3cd167f0f8fe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '38d59822-a08e-4dfc-bd23-c68f22d453d3'
+};
